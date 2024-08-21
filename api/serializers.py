e192b09f138a59e78b8401c679734b32c1c36b20
preritagrawal06 <agrawalprerit780@gmail.com>
2024-08-22 00:07:07 +0530

    Added emailField validation

--- api/serializers.py
+++ api/serializers.py
@@ -1,15 +1,17 @@
 from rest_framework import serializers
 from .models import Todo
-from django.contrib.auth.models import User
-
+from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueValidator
 class UserSerializer(serializers.ModelSerializer):
+    activeUser = get_user_model()
+    email = serializers.EmailField(validators=[UniqueValidator(queryset=activeUser.objects.all())])
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ["id", "username", "email", "password"]
         extra_kwargs = {'password': {"write_only": True}}
         
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
         return user
 
 class TodoSerializer(serializers.ModelSerializer):
